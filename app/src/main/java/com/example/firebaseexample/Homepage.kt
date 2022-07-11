948cfd12c03fd7efed91017f62afd1417dcd6c84
dunglongchan <62632444+dunglongchan@users.noreply.github.com>
2022-07-11 17:49:49 +0700

    Firebase Google signin, notification, crashlytics

--- app/src/main/java/com/example/firebaseexample/Homepage.kt
+++ app/src/main/java/com/example/firebaseexample/Homepage.kt
@@ -1,18 +1,31 @@
 package com.example.firebaseexample
 
+import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.ads.nativetemplates.NativeTemplateStyle
+import com.google.android.ads.nativetemplates.TemplateView
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.android.gms.ads.nativead.NativeAd
+import com.google.android.gms.ads.rewarded.RewardItem
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_homepage.*
 
+
 class Homepage : AppCompatActivity() {
+
+    private var mInterstitialAd: InterstitialAd? = null
+    private var mRewardedAd: RewardedAd? = null
 
     lateinit var gsc: GoogleSignInClient
     private val auth by lazy {
@@ -43,9 +56,82 @@ class Homepage : AppCompatActivity() {
         crashapp.setOnClickListener {
             throw RuntimeException("Test Crash") // Force a crash
         }
-//        addContentView(crashapp, ViewGroup.LayoutParams(
-//            ViewGroup.LayoutParams.MATCH_PARENT,
-//            ViewGroup.LayoutParams.WRAP_CONTENT))
+
+        //adview - banner
+        MobileAds.initialize(this)
+        val adRequest = AdRequest.Builder().build()
+        adv_banner.loadAd(adRequest)
+
+        //adview - interstitial
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-3940256099942544/1033173712",
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    adError?.toString()?.let { Log.d("abc", it) }
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    Log.d("abc", "Ad was loaded.")
+                    mInterstitialAd = interstitialAd
+                }
+            })
+
+        interstitial_show.setOnClickListener {
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(this)
+            } else {
+                Log.d("TAG", "The interstitial ad wasn't ready yet.")
+            }
+        }
+
+        //adview - rewarded
+        RewardedAd.load(
+            this,
+            "ca-app-pub-3940256099942544/5224354917",
+            adRequest,
+            object : RewardedAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    adError?.toString()?.let { Log.d(TAG, it) }
+                    mRewardedAd = null
+                }
+
+                override fun onAdLoaded(rewardedAd: RewardedAd) {
+                    Log.d(TAG, "Ad was loaded.")
+                    mRewardedAd = rewardedAd
+                }
+            })
+
+        rewarded_show.setOnClickListener {
+            if (mRewardedAd != null) {
+                mRewardedAd?.show(this, OnUserEarnedRewardListener() {
+                    fun onUserEarnedReward(rewardItem: RewardItem) {
+                        var rewardAmount = rewardItem.amount
+                        var rewardType = rewardItem.type
+                        Log.d(TAG, "User earned the reward.")
+                    }
+                })
+            } else {
+                Log.d(TAG, "The rewarded ad wasn't ready yet.")
+            }
+        }
+
+
+        //adview - native
+
+        val adLoader: AdLoader = AdLoader.Builder(this, "ca-app-pub-3940256099942544/2247696110")
+            .forNativeAd { nativeAd ->
+                val styles =
+                    NativeTemplateStyle.Builder().build()
+                val template = findViewById<TemplateView>(R.id.my_template)
+                template.setStyles(styles)
+                template.setNativeAd(nativeAd)
+            }
+            .build()
+
+        adLoader.loadAd(AdRequest.Builder().build())
 
     }
 }
