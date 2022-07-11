ffdf375f77f8db38b5f9c279486b4a2b5a7588f0
dunglongchan <62632444+dunglongchan@users.noreply.github.com>
2022-07-11 15:25:36 +0700

    Firebase Google signin, notification, crashlytics

--- app/src/main/java/com/example/firebaseexample/Homepage.kt
+++ app/src/main/java/com/example/firebaseexample/Homepage.kt
@@ -1,12 +1,16 @@
 package com.example.firebaseexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_homepage.*
 
 class Homepage : AppCompatActivity() {
 
@@ -20,6 +24,28 @@ class Homepage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        gsc = GoogleSignIn.getClient(this, gso)
+
+        logout.setOnClickListener {
+            gsc.signOut().addOnCompleteListener {
+                val intent = Intent(this, MainActivity::class.java)
+                Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        crashapp.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+//        addContentView(crashapp, ViewGroup.LayoutParams(
+//            ViewGroup.LayoutParams.MATCH_PARENT,
+//            ViewGroup.LayoutParams.WRAP_CONTENT))
 
     }
 }
